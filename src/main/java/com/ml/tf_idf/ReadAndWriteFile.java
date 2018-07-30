bbb058aad3abc6cdd5c029cad3bae0e7fcd2ae07
wuyuwu <839427538@qq.com>
2018-07-30 18:01:36 +0800

    udpate

---
+++ src/main/java/com/ml/tf_idf/ReadAndWriteFile.java
@@ -0,0 +1,9 @@
+package com.ml.tf_idf;
+
+
+/**
+ * 读写文件
+ * 实现对文件的读写功能
+ */
+public class ReadAndWriteFile {
+}
---
+++ src/main/java/com/ml/tf_idf/TfIdf.java
@@ -0,0 +1,18 @@
+package com.ml.tf_idf;
+
+import java.util.Map;
+
+/**
+ * 实现tf
+ * 实现idf
+ */
+public class TfIdf {
+
+    public Map<String,Integer> Tf(){
+        return null;
+    }
+
+    public Map<String,Double> Idf(){
+        return null;
+    }
+}
