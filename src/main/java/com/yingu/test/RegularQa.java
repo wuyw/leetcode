6e7fcf3d64723ca35cea78344726a50b7eea594c
wuyuwu <839427538@qq.com>
2018-07-30 18:16:10 +0800

    add file

---
+++ src/main/java/com/yingu/test/RegularQa.java
@@ -0,0 +1,27 @@
+package com.yingu.test;
+
+
+/**
+ * 格式为
+ * Q:xxxxxxx
+ * A:xxxxxxx
+ * 格式化为CSV 文件
+ */
+public class RegularQa {
+
+    public static void main(String[] args) {
+
+        /**
+         *  将文本文件格式为csv文件
+         */
+
+
+
+
+
+        /**
+         * 将文件格式化为json 格式数据
+         */
+
+    }
+}
